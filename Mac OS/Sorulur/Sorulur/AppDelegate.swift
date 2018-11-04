d24c2dc0376d2aa54eebc7816ead547c7cc83e64
sozman <sinanozman@hotmail.com>
2018-11-04 13:58:25 +0300

    Mac Os application init

---
+++ Mac OS/Sorulur/Sorulur/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Sorulur
+//
+//  Created by Sinan Özman on 4.11.2018.
+//  Copyright © 2018 Sinan Özman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Mac OS/Sorulur/Sorulur/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sorulur
+//
+//  Created by Sinan Özman on 4.11.2018.
+//  Copyright © 2018 Sinan Özman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
